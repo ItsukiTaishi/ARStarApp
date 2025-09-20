41d953aac72447cec1c2f5ca0423798f5ea5ed0d
ItsukiTaishi <ituki.sao.taisi@gmail.com>
2025-09-20 20:19:37 +0900

    Initial Commit

---
+++ Gassyuku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gassyuku
+//
+//  Created by hasegawa on 2025/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gassyuku/GassyukuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GassyukuApp.swift
+//  Gassyuku
+//
+//  Created by hasegawa on 2025/09/20.
+//
+
+import SwiftUI
+
+@main
+struct GassyukuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
