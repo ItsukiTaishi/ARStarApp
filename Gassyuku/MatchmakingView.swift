8f2b76b57bbf32986e4908479fa8d2e4964fff00
ItsukiTaishi <ituki.sao.taisi@gmail.com>
2025-09-22 17:28:18 +0900

    基本レイアウトを作成して、AR機能およびgamekitのマッチング機能を実装

---
+++ Gassyuku/MatchmakingView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectUserView.swift
+//  Gassyuku
+//
+//  Created by hasegawa on 2025/09/22.
+//
+
+import SwiftUI
+
+struct ConnectUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectUserView()
+}
---
+++ Gassyuku/StarView.swift
@@ -0,0 +1,18 @@
+//
+//  StarView.swift
+//  Gassyuku
+//
+//  Created by hasegawa on 2025/09/22.
+//
+
+import SwiftUI
+
+struct StarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarView()
+}
